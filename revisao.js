4a4b9894d3e5b3daaa21dd9f2586b209764b02d3
Gisele Santos <giseledosantos@outlook.com>
2021-10-15 20:16:01 -0300

    atualizações gerais

---
+++ revisao.js
@@ -0,0 +1,23 @@
+/*const aluna = new Object()
+aluna.nome = "Nayara"
+aluna.curso = "frontend"
+
+aluna = {
+    nome: "Nayara",
+    curso:"frontend"
+}*/
+
+//outra forma de criar objeto
+const monitora ={
+    nome: "Thiele",
+    curso:"frontend"
+}
+monitora.cidade = "Barueri"
+
+//console.log(monitora.cidade)
+
+
+const alunas = new Array('Julia', 'Celina', 'Bruna')
+const cursos = ['frontend', 'backend']
+
+console.log(alunas.length)
