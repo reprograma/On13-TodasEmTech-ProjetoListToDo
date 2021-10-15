ad1d98a8f3ec1ce00eaae6909bb523b28d2b3259
Taina <taiaguilera@gmail.com>
2021-10-14 22:42:08 -0300

    Entendendo enfim javascrit

---
+++ revisao.js
@@ -0,0 +1,12 @@
+const aluna = new Object()
+aluna.nome = "Lilit"
+aluna.curso = "frontend"
+aluna.cidade = "Recife"
+
+const monitora = {
+  nome: "Carol",
+  curso: "frontend",
+  cidade: "Brasília",
+}
+console.log(aluna.curso)
+console.log(monitora.nome)
