7f369102c3af90c8302891df1d9731c1dab968b5
Aline Viana <alinemaciel018@gmail.com>
2021-10-16 09:32:53 -0300

    adicionando atividade da semana

---
+++ aline-viana/script.js
@@ -0,0 +1,95 @@
+// 1. Capturar entradas e armazenar em variáveis
+
+const novaTarefa = document.querySelector("#input_id");
+const botaoAdd = document.getElementById('botao_add_id');
+const formulario = document.getElementById('form_id');
+const listaDeTarefas = document.getElementById('lista_id');
+const botaoMarca = document.getElementById('botao_marca_id');
+const botaoLimpa = document.getElementById('botao_limpa_id');
+
+// 2. Processamentos através de eventos e funções
+
+// função de adicionar a partir do click da usuária, um evento
+botaoAdd.addEventListener('click', (event) => {
+  event.preventDefault()
+
+  const elementoLista = document.createElement('li')
+  const textoTarefa = document.createElement('p')
+  const iconeDeleta = document.createElement('span')
+
+  textoTarefa.innerText = novaTarefa.value;
+  iconeDeleta.innerText = "🗑"
+
+  if(textoTarefa.innerText.trim() === '') {
+    alert('Você precisa digitar alguma coisa!')
+    /* DESAFIO 1
+    novaTarefa.classList.add('erro'); //adiciona classe erro ao elemento novaTarefa (input) - a classe está no css
+    novaTarefa.addEventListener('animationend', event => { 
+      novaTarefa.classList.remove('erro');
+    }) */
+  } else {
+    listaDeTarefas.appendChild(elementoLista)
+    elementoLista.appendChild(textoTarefa)
+    elementoLista.appendChild(iconeDeleta)
+    formulario.reset()
+  }
+  novaTarefa.focus()
+
+  // função para marcar individualmente uma tarefa
+  textoTarefa.addEventListener('click', () => {
+    // if(!textoTarefa.classList.contains('checked')) {
+    //   textoTarefa.classList.add('checked')
+    // } else {
+    //   textoTarefa.classList.remove('checked')
+    // }
+    textoTarefa.classList.toggle('checked')
+    verificarMarcados() // chama a função do DESAFIO 2 para mudar o texto do botão quando marcar um item individual, se necessário
+  })
+
+  // função para deletar individualmente uma terefa
+  iconeDeleta.addEventListener('click', () => {
+    // elementoLista.removeChild(elementoLista)
+    elementoLista.remove();
+  })
+  verificarMarcados() // chama a função do DESAFIO 2 para mudar o texto do botão quando adicionar nova tarefa, se necessário
+})
+
+// função para marcar todas as tarefas:
+
+botaoMarca.addEventListener('click', () => {
+  let todasAsTarefas = document.querySelectorAll('p')
+
+  if(botaoMarca.innerText === "Marcar todos") {
+    todasAsTarefas.forEach((tarefa) => {
+      tarefa.classList.add("checked")
+    })
+    botaoMarca.innerText = "Desmarcar todos"
+  } else {
+    todasAsTarefas.forEach((tarefa) => {
+      tarefa.classList.remove("checked")
+    })
+    botaoMarca.innerText = "Marcar todos"
+  }
+})
+
+
+botaoLimpa.addEventListener('click', () => {
+  listaDeTarefas.innerHTML = '';
+})
+
+function verificarMarcados() {
+  let todasAsTarefas = document.querySelectorAll('p')
+  let verificacao = [] 
+
+  todasAsTarefas.forEach(tarefa => { 
+    if(!tarefa.classList.contains("checked")){ 
+      verificacao.push(tarefa)
+    };
+  })
+  
+  if(verificacao.length === 0 && todasAsTarefas) { 
+    botaoMarca.innerText = "Desmarcar todos"  
+  } else { 
+    botaoMarca.innerText = "Marcar todos"
+  }
+} 
