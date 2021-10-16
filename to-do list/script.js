8b4709f8c1dca2d38e9c375cdffb2d4f8b421fe1
AidaFig <aidafigueiredo8@gmail.com>
2021-10-16 09:07:30 -0300

    Atualizando resolução da tarefa

--- to-do list/script.js
+++ to-do list/script.js
@@ -1,82 +1,72 @@
-// 1. Capturar entradas e armazenar em varáveis
+// 1. Capturar entradas e armazenar em variáveis
 
-const novaTarefa = document.querySelector('#input_id');
-const botaoAdicionar = document.getElementById('botao_add_id');
+const novaTarefa = document.querySelector("#input_id");
+const botaoAdd = document.getElementById('botao_add_id');
 const formulario = document.getElementById('form_id');
 const listaDeTarefas = document.getElementById('lista_id');
 const botaoMarca = document.getElementById('botao_marca_id');
 const botaoLimpa = document.getElementById('botao_limpa_id');
 
-// 2. Processamento através de eventos e funções
+// 2. Processamentos através de eventos e funções
 
-//função de adicionar a partir do clique da usuária, um evento
-botaoAdicionar.addEventListener('click', (event) => {
-    event.preventDefault()
+// função de adicionar a partir do click da usuária, um evento
+botaoAdd.addEventListener('click', (event) => {
+  event.preventDefault()
+
+  const elementoLista = document.createElement('li')
+  const textoTarefa = document.createElement('p')
+  const iconeDeleta = document.createElement('span')
+
+  textoTarefa.innerText = novaTarefa.value;
+  iconeDeleta.innerText = "🗑"
+
+  if(textoTarefa.innerText.trim() === '') {
+    alert('Você precisa digitar alguma coisa!')
     
-    const elementoLista = document.createElement('li');
-    const textoTarefa = document.createElement('p');
-    const iconeDeleta = document.createElement('span');
+  } else {
+    listaDeTarefas.appendChild(elementoLista)
+    elementoLista.appendChild(textoTarefa)
+    elementoLista.appendChild(iconeDeleta)
+    formulario.reset()
+  }
+  novaTarefa.focus()
 
+  // função para marcar individualmente uma tarefa
+  textoTarefa.addEventListener('click', () => {
     
-    textoTarefa.innerText = novaTarefa.value
-    iconeDeleta.innerHTML = "🗑️"
+    textoTarefa.classList.toggle('checked')
     
-    if(textoTarefa.innerText.trim() === '') {
-        alert('Você precisa digitar alguma coisa')
-    } else {
-        listaDeTarefas.appendChild(elementoLista)
-        elementoLista.appendChild(textoTarefa)
-        elementoLista.appendChild(iconeDeleta)
-        formulario.reset()
-    }
-    
-    novaTarefa.focus()
-})
+  })
 
-
-//função para marcar individualmente uma tarefa
-
-textoTarefa.addEventListener('click', () => {
-    textoTarefa.classList.toggle('checked');
-    
-})
-
-
-//função para deletar individualmente uma tarefa
-
-iconeDeleta.addEventListener('click', () => {
+  // função para deletar individualmente uma terefa
+  iconeDeleta.addEventListener('click', () => {
+    // elementoLista.removeChild(elementoLista)
     elementoLista.remove();
+  })
+  
 })
-//função para marcar para o botao que marca
+
+// função para marcar todas as tarefas:
 
 botaoMarca.addEventListener('click', () => {
-    let todasAsTarefas = document.querySelectorAll('p')
-    if(botaoMarca.innerText === "Marcar todos") {
-        todasAsTarefas.forEach( (tarefa) => {
-            tarefa.classList.add("ckecked");
-        })
-        botaoMarca.innerText = "Desmarcar todos"
-    }else {
-        todasAsTarefas.forEach((tarefa) => {
-            tarefa.classList.remove();
-        })
-        botaoMarca.innerText = "Marcar todos"
-    }
+  let todasAsTarefas = document.querySelectorAll('p')
+
+  if(botaoMarca.innerText === "Marcar todos") {
+    todasAsTarefas.forEach((tarefa) => {
+      tarefa.classList.add("checked")
+    })
+    botaoMarca.innerText = "Desmarcar todos"
+  } else {
+    todasAsTarefas.forEach((tarefa) => {
+      tarefa.classList.remove("checked")
+    })
+    botaoMarca.innerText = "Marcar todos"
+  }
 })
 
-//função para limpar todas as tarefas
+// função para limpar todas as tarefas:
+
 botaoLimpa.addEventListener('click', () => {
-    listaDeTarefas.innerHTML = ''
+  listaDeTarefas.innerHTML = '';
 })
 
-
-
-
-// console.log(
-//     novaTarefa,
-//     botaoAdicionar,
-//     formulario,
-//     listaDeTarefas,
-//     botaoMarca,
-//     botaoLimpa,
-//     )
