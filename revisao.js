7ced74013356904236061e6ef357f26c6b1ed7fb
valentinahastenreiter <valentina.hastenreiter@gmail.com>
2021-10-17 19:05:01 -0300

    Finalização do Projeto ToDo Lista

---
+++ revisao.js
@@ -0,0 +1,15 @@
+
+// const monitora = {
+//     nome: 'thiele',
+//     curso: 'Frontend'
+// }
+
+// console.log(monitora.nome)
+
+// console.log(monitora.cidade)
+
+const alunas = new Array ('Julia', 'Celina', 'Bruna')
+const cursos = ['frontend', 'backend']
+
+console.log(alunas.length)
+
